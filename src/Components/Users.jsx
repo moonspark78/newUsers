ada148134108670853a022383255b4915505f1ab
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-09 02:12:30 +0100

    :construction: test with tailwind  :construction:

---
+++ src/Components/Users.jsx
@@ -0,0 +1,16 @@
+import { useState,useEffect } from 'react'
+
+export const Users = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() =>{},[]);
+
+  return (
+    <div>
+        <h2 className='text-3xl underline'>Users</h2>
+        <div>
+
+        </div>
+    </div>
+  )
+}
