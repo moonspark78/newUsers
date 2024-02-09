bf96a48c4f037759252910826c357508aad7bfc0
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-09 03:43:11 +0100

    :construction: test with tailwind  :construction:

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,30 +1,31 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export const Users = () => {
-    const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-    useEffect(() =>{
-        const fetchData = async () => {
-            const reponse = await fetch("https://jsonplaceholder.typicode.com/users");
-            const data = await reponse.json();
-            console.log(data);
-            setUsers(data);
-        };
-        fetchData();
-    },[]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const reponse = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await reponse.json();
+      console.log(data);
+      setUsers(data);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div className='text-center'>
-        <h2 className='text-3xl underline'>Users</h2>
-        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto max-w-screen-lg'>
-            {
-                users.map(user =>(
-                    <div key={user.id} className='rounded-md p-4 shadow-md'>
-                        <p>{user.name}</p>
-                    </div>
-                ))
-            }
-        </div>
+    <div className="text-center">
+      <h2 className="text-3xl underline">Users</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto max-w-screen-lg">
+        {users.map((user) => (
+          <div key={user.id} className="rounded-md p-4 shadow-md w-full md:w-auto">
+            <p className="text-black">{user.name}</p>
+            <p className="text-black">{user.email}</p>
+            <p className="text-black">{user.phone}</p>
+            <p className="text-black">{user.website}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
