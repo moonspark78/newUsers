87ce9d2838edc6078ae92c12888fbea890a73781
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-09 02:46:41 +0100

    :construction: test with tailwind  :construction:

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -3,13 +3,25 @@ import { useState,useEffect } from 'react'
 export const Users = () => {
     const [users, setUsers] = useState([])
 
-    useEffect(() =>{},[]);
+    useEffect(() =>{
+        const fetchData = async () => {
+            const reponse = await fetch("https://jsonplaceholder.typicode.com/users");
+            const data = await reponse.json();
+            console.log(data);
+            setUsers(data);
+        };
+        fetchData();
+    },[]);
 
   return (
     <div>
         <h2 className='text-3xl underline'>Users</h2>
         <div>
-
+            {
+                users.map(user =>(
+                    <p>{user.name}</p>
+                ))
+            }
         </div>
     </div>
   )
